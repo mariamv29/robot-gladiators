51bff9763301179f69c430b6f6e97eaae59871bd
Maria Mireles <mireles335@gmail.com>
2021-05-17 23:29:59 -0500

    functions

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,18 +1,21 @@
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 10;
-var playMoney = 10;
+var playerMoney = 10;
+
 
 // You can also log multiple values at once like this
 console.log(playerName, playerAttack, playerHealth);
 
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
+
 ///fight function
-var fight = function () {
-  // Alert players that they are starting the round
+
+var fight = function (enemyName) {
+  // Alert players that they are starting the round // fight function statements
   window.alert("Welcome to Robot Gladiators!");
 
   var promptFight = window.prompt(
@@ -71,8 +74,9 @@ var fight = function () {
     }
     // if no (false), ask question again by running fight() again
     else {
-      fight();
-    }
+        for(var i = 0; i < enemyNames.length; i++) {
+            fight(enemyNames[i]);
+          }
   }
 };
-fight();
+// fight();
