f4f6f145855bfcca208aaa3e8accd37d8381b675
Maria Mireles <mireles335@gmail.com>
2021-05-24 13:00:24 -0500

    fight-skip bug

--- assets/js/game.js
+++ assets/js/game.js
@@ -5,42 +5,55 @@ var randomNumber = function (min, max) {
   return value;
 };
 
-var fightOrSkip = function() {
-  // ask player if they'd like to fight or skip using fightOrSkip function
-  var promptFight = window.prompt('Would you like to FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose.');
+// function to check if player wants to fight or skip
+var fightOrSkip = function () {
+  // ask player if they'd like to fight or run
+  var promptFight = window.prompt(
+    'Would you like to FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose.'
+  );
 
-  // Conditional Recursive Function Call
-if (promptFight === "" || promptFight === null) {
-  window.alert("You need to provide a valid answer! Please try again.");
-  return fightOrSkip();
-}
-  // if player picks "skip" confirm and then stop the loop
-  if (promptFight === "skip" || promptFight === "SKIP") {
+  // validate prompt answer
+  if (promptFight === "" || promptFight === null) {
+    window.alert("You need to provide a valid answer! Please try again.");
+    // use return to call it again and stop the rest of this function from running
+    return fightOrSkip();
+  }
+
+  // convert promptFight to all lowercase so we can check with less options
+  promptFight = promptFight.toLowerCase();
+
+  if (promptFight === "skip") {
     // confirm player wants to skip
     var confirmSkip = window.confirm("Are you sure you'd like to quit?");
 
     // if yes (true), leave fight
     if (confirmSkip) {
-      window.alert(playerInfo.name + " has decided to skip this fight. Goodbye!");
-      // subtract money from playerMoney for skipping
-      playerInfo.playerMoney = playerInfo.money - 10;
-      shop();
+      window.alert(
+        playerInfo.name + " has decided to skip this fight. Goodbye!"
+      );
+      // subtract money from playerMoney for skipping, but don't let them go into the negative
+      playerInfo.money = Math.max(0, playerInfo.money - 10);
+      // stop while() loop using break; and enter next fight
+
+      // return true if player wants to leave
+      return true;
     }
   }
-}
+  return false;
+};
 
-
-///fight function
+// fight function (now with parameter for enemy's object holding name, health, and attack values)
 var fight = function (enemy) {
-  // repeat and execute as long as the enemy-robot is alive 
-while (playerInfo.health > 0 && enemy.health > 0) {
-  fightOrSkip(); // <-- Replace code with this function call
-  var damage = randomNumber(playerInfo.attack - 3, playerInfo.attack);
+  while (playerInfo.health > 0 && enemy.health > 0) {
+    // ask player if they'd like to fight or skip using fightOrSkip function
+    if (fightOrSkip()) {
+      // if true, leave fight by breaking loop
+      break;
     }
 
-    // generate random damage value based on player's attack power
     var damage = randomNumber(playerInfo.attack - 3, playerInfo.attack);
 
+    // remove enemy's health by subtracting the amount we set in the damage variable
     enemy.health = Math.max(0, enemy.health - damage);
     console.log(
       playerInfo.name +
@@ -52,6 +65,7 @@ while (playerInfo.health > 0 && enemy.health > 0) {
         enemy.health +
         " health remaining."
     );
+
     // check enemy's health
     if (enemy.health <= 0) {
       window.alert(enemy.name + " has died!");
@@ -64,9 +78,10 @@ while (playerInfo.health > 0 && enemy.health > 0) {
     } else {
       window.alert(enemy.name + " still has " + enemy.health + " health left.");
     }
-
-    // remove players's health by subtracting the amount set in the enemy.attack variable
+    /// remove players's health by subtracting the amount set in the enemy.attack variable
     var damage = randomNumber(enemy.attack - 3, enemy.attack);
+
+    // remove enemy's health by subtracting the amount we set in the damage variable
     playerInfo.health = Math.max(0, playerInfo.health - enemy.attack);
     console.log(
       enemy.name +
@@ -88,6 +103,7 @@ while (playerInfo.health > 0 && enemy.health > 0) {
       window.alert(
         playerInfo.name + " still has " + playerInfo.health + " health left."
       );
+    }
   }
 };
 
